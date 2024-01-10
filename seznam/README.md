63a393e9c12b51f42b40c80cf97a56ffd04ae272
bi4528 <36712965+bi4528@users.noreply.github.com>
2024-01-10 03:00:03 +0100

    delujoče funkcionalnosti na cloudu

--- seznam/README.md
+++ seznam/README.md
@@ -3,7 +3,7 @@
 ## Prerequisites
 
 ```bash
-docker run -d --name pg-seznam-metadata -e POSTGRES_USER=dbuser -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=seznam-metadata -p 5433:5432 postgres:13
+docker run -d --name pg-seznam-metadata -e POSTGRES_USER=dbuser -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=seznam-metadata -p 5432:5432 postgres:13
 ```
 
 ## Build and run commands
@@ -12,7 +12,7 @@ mvn clean package
 cd api/target
 java -jar seznam-api-1.0.0-SNAPSHOT.jar
 ```
-Available at: localhost:8081/v1/seznam
+Available at: localhost:8080/v1/seznam
 
 ## Docker commands
 ```bash
--- seznam/services/src/main/java/si/fri/rso/seznam/services/config/AppConfig.java
+++ seznam/services/src/main/java/si/fri/rso/seznam/services/config/AppConfig.java
@@ -1,18 +1,28 @@
 package si.fri.rso.seznam.services.config;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import si.fri.rso.seznam.services.beans.SeznamMetadataBean;
 
 import javax.enterprise.context.ApplicationScoped;
+import java.util.logging.Logger;
 
 @ApplicationScoped
 public class AppConfig {
     private String koktejliServiceUrl;
 
+    private Logger log = Logger.getLogger(AppConfig.class.getName());
+
     public AppConfig() {
-        Dotenv dotenv = Dotenv.configure()
-                .directory("../../../.env")
-                .load();
-        koktejliServiceUrl = dotenv.get("KOKTEJLI_SERVICE_URL", "http://localhost:8080/v1/koktejli");
+        log.info("Poskusam prebrati okoljsko spremenljivko KOKTEJLI_SERVICE_URL");
+        // Poskusite prebrati okoljsko spremenljivko
+        //koktejliServiceUrl = System.getenv("KOKTEJLI_SERVICE_URL");
+        koktejliServiceUrl = null;
+
+        // Če okoljska spremenljivka ni nastavljena, preberite iz .env datoteke
+        if (koktejliServiceUrl == null) {
+            koktejliServiceUrl = "http://koktejli-service:8080/v1/koktejli";
+        }
+        log.info("Koktejli service URL: " + koktejliServiceUrl);
     }
 
     public String getKoktejliServiceUrl() {
