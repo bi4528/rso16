b899628d7fec59d9e033e54db14530c976861db9
bi4528 <36712965+bi4528@users.noreply.github.com>
2024-01-11 16:58:35 +0100

    openapi public address

--- seznam/api/src/main/java/si/fri/rso/seznam/api/v1/SeznamMetadataApplication.java
+++ seznam/api/src/main/java/si/fri/rso/seznam/api/v1/SeznamMetadataApplication.java
@@ -13,7 +13,7 @@ import org.eclipse.microprofile.openapi.annotations.servers.Server;
 @OpenAPIDefinition(info = @Info(title = "User cocktail list API", version = "v1",
         contact = @Contact(email = "bi4528@student.uni-lj.si"),
         license = @License(name = "dev"), description = "API for managing User cocktail list."),
-        servers = @Server(url = "http://localhost:8080/"))
+        servers = @Server(url = "http://34.89.140.13:8080/"))
 @ApplicationPath("/v1")
 public class SeznamMetadataApplication extends Application {
 
